09e9dc668c6200b506fa54d3c2ce177737feb9c2
Yemelyanov <nikolaef.post@gmail.com>
2022-01-25 18:21:17 +0200

    added form validations

--- src/types/types.ts
+++ src/types/types.ts
@@ -1,32 +1,10 @@
-// export interface IGeo{
-//     lat: string;
-//     lng: string;
-// }
-//
-// export interface IAddress{
-//     street: string;
-//     suite: string;
-//     city: string;
-//     zipcode: string;
-//     geo: IGeo;
-// }
-//
-// export interface ICompany{
-//     name: string;
-//     catchPhrase: string;
-//     bs: string
-// }
 
 export interface IUser{
     id: number;
     name: string;
     username: string;
-    // email: string;
-    // address: IAddress
     phone: string| number;
     img?: string|null;
-    // website: string;
-    // company: ICompany
 }
 
 export interface ContactState {
